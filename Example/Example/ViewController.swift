0a049b46a59c16ab77027ede5f93040b7b50f63f
rzmn <agerfirelol@gmail.com>
2017-11-06 01:46:12 +0300

    fixed untracked files

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -7,19 +7,40 @@
 //
 
 import UIKit
+import CropView
 
 class ViewController: UIViewController {
 
+    let cropView = SECropView()
+    let imageView : UIImageView = {
+        let view = UIImageView()
+        view.contentMode = .scaleAspectFit
+        view.isUserInteractionEnabled = true
+        view.image = #imageLiteral(resourceName: "note.jpg")
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = UIColor.white
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        imageView.frame = view.frame
+        cropView.frame = view.frame
+        cropView.configureWithCorners(corners: [CGPoint(x: 10, y: 10),
+                                                CGPoint(x: 10, y: 100),
+                                                CGPoint(x: 150, y: 150),
+                                                CGPoint(x: 100, y: 10)])
+        view.addSubview(imageView)
+        imageView.addSubview(cropView)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
 
 }
 
