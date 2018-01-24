e9a319c99e55c8548088cc29fc99bd6c90fcd3c2
Никита Разумный <rzmn@Nikitas-MacBook-Pro-3.local>
2018-01-24 22:22:04 +0300

    add autolayout sample

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -11,31 +11,14 @@ import CropView
 
 class ViewController: UIViewController {
 
-    let cropView = SECropView()
-    let imageView : UIImageView = {
-        let view = UIImageView()
-        view.contentMode = .scaleAspectFit
-        view.isUserInteractionEnabled = true
-        view.image = #imageLiteral(resourceName: "paper.jpg")
-        return view
-    }()
+    @IBOutlet weak var cropView: SECropView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.white
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        imageView.frame = view.frame
-        cropView.frame = view.frame
         cropView.configureWithCorners(corners: [CGPoint(x: 100, y: 120),
                                                 CGPoint(x: 270, y: 170),
                                                 CGPoint(x: 280, y: 450),
                                                 CGPoint(x: 120, y: 400)])
-        view.addSubview(imageView)
-        imageView.addSubview(cropView)
     }
     
     override func didReceiveMemoryWarning() {
--- Pod/Classes/SECornerView.swift
+++ Pod/Classes/SECornerView.swift
@@ -21,7 +21,7 @@ class SECornerView: UIView {
     }
     
     required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: aDecoder)
     }
     
     override func draw(_ rect: CGRect) {
