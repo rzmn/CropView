ad7262c44cab8946a64b67d56f1196b6785235d0
rzmn <agerfirelol@gmail.com>
2018-02-10 17:43:36 +0300

    changed sample

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -8,30 +8,29 @@
 
 import UIKit
 import CropView
+import AVFoundation
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var cropView: SECropView!
+    let cropView = SECropView()
     @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         cropView.configureWithCorners(corners: [CGPoint(x: 100, y: 120),
                                                 CGPoint(x: 270, y: 170),
                                                 CGPoint(x: 280, y: 450),
-                                                CGPoint(x: 120, y: 400)])
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+                                                CGPoint(x: 120, y: 400)], on: imageView)
     }
     
     @IBAction func saveImg(_ sender: Any) {
         do {
-            let quad = try SEQuadrangleHelper.getCoordinatesOnImageWithoutScale(on: imageView, with: cropView)
-            print(quad)
+            guard let corners = cropView.cornerLocations else { return }
             guard let image = imageView.image else { return }
-            let croppedImage = try SEQuadrangleHelper.cropImage(with: image, quad: quad)
+            
+            let croppedImage = try SEQuadrangleHelper.cropImage(with: image, quad: corners)
+            
             performSegue(withIdentifier: "doCrop", sender: croppedImage)
         } catch let error as SECropError {
             print(error)
