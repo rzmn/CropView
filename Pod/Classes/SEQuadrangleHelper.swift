aaac1cfec82484aedafc4a143d83f6081cf21fa2
rzmn <agerfirelol@gmail.com>
2018-02-10 17:42:56 +0300

    removed useless methods, realized it in cropview

--- Pod/Classes/SEQuadrangleHelper.swift
+++ Pod/Classes/SEQuadrangleHelper.swift
@@ -59,48 +59,20 @@ public class SEQuadrangleHelper {
         
         guard let transform = perspectiveCorrection else { throw SECropError.unknown }
         transform.setValue(CIVector(cgPoint: orderedQuad[0].cartesian(for: imgSize)),
-                                       forKey: "inputTopLeft")
+                           forKey: "inputTopLeft")
         transform.setValue(CIVector(cgPoint: orderedQuad[1].cartesian(for: imgSize)),
-                                       forKey: "inputTopRight")
+                           forKey: "inputTopRight")
         transform.setValue(CIVector(cgPoint: orderedQuad[2].cartesian(for: imgSize)),
-                                       forKey: "inputBottomRight")
+                           forKey: "inputBottomRight")
         transform.setValue(CIVector(cgPoint: orderedQuad[3].cartesian(for: imgSize)),
-                                    forKey: "inputBottomLeft")
+                           forKey: "inputBottomLeft")
         transform.setValue(ciImage, forKey: kCIInputImageKey)
         
         guard let perspectiveCorrectedImg = transform.outputImage else { throw SECropError.unknown }
         
         return UIImage(ciImage: perspectiveCorrectedImg)
     }
-    
-    // TODO: works only when image content mode is aspectFit
-    static public func getCoordinatesOnImageWithoutScale(on imageView: UIImageView, with cropView: SECropView) throws -> Array<CGPoint> {
-        let cornersOnImageView = try getCoordinatesOnImageView(on: imageView, with: cropView)
-        
-        guard let image = imageView.image else { throw SECropError.missingImageOnImageView }
-        
-        let imageFrame = AVMakeRect(aspectRatio: image.size, insideRect: imageView.bounds)
-        let imageOrigin = imageFrame.origin
-        let scale = image.size.height * image.scale / imageFrame.height
-        
-        let cornersOnImage : [CGPoint] = cornersOnImageView.map{ CGPoint(x: ($0.x - imageOrigin.x) * scale,
-                                                                         y: ($0.y - imageOrigin.y) * scale) }
-        
-        return cornersOnImage
-    }
-    
-    static public func getCoordinatesOnImageView(on imageView: UIImageView, with cropView: SECropView) throws -> Array<CGPoint> {
-        guard let imageViewGlobalOrigin = imageView.globalPoint else { throw SECropError.missingSuperview }
-        guard let cropViewGlobalOrigin  = cropView.globalPoint  else { throw SECropError.missingSuperview }
 
-        let corners = cropView.cornerLocations
-        let cornersOnImageView : [CGPoint] = corners.map{ CGPoint(x: $0.x - cropViewGlobalOrigin.x + imageViewGlobalOrigin.x,
-                                                                  y: $0.y - cropViewGlobalOrigin.y + imageViewGlobalOrigin.y) }
-        
-        return cornersOnImageView
-    }
-    
-    
     static internal func checkConvex(corners: [CGPoint]) -> Bool {
         guard corners.count > 2 else {
             return false
